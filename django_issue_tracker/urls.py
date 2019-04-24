9f30220deada1faa67820ca434a9acc9c0876707
Alex <alexthirlwell123@gmail.com>
2019-04-24 22:21:25 +0000

    Added password reset logic and cleaned urls

--- django_issue_tracker/urls.py
+++ django_issue_tracker/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts import urls as accounts_urls
+from admin import urls as admin_urls
+from admin.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include(accounts_urls)),
+    url(r'^accounts/', include(admin_urls)),
+    url(r'^$', index, name="index"),
 ]
