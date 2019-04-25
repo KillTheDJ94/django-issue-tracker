a09ec868258e030b3b55de7cc35c4d75dd94fb7c
Alex <alexthirlwell123@gmail.com>
2019-04-25 11:45:04 +0000

    Added urls and links for user profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration
+from accounts.views import logout, login, registration, user_bug_profile, user_feature_profile
 from accounts import urls_reset
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', registration, name="registration"),
     url(r'^password-reset/', include(urls_reset)),
+    url(r'^user_bug_profile/', user_bug_profile, name="user_bug_profile"),
+    url(r'^user_feature_profile/', user_feature_profile, name="user_feature_profile"),
     ]
