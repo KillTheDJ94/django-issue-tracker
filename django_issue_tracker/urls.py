d1bdfdb146e4317636de178658e8819e6838fb57
Alex <alexthirlwell123@gmail.com>
2019-04-25 09:27:04 +0000

    Added urls to blog app and cleaned base urls

--- django_issue_tracker/urls.py
+++ django_issue_tracker/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts import urls as accounts_urls
 from accounts.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include(accounts_urls)),
+    url(r'^accounts/', include('accounts_urls')),
     url(r'^$', index, name="index"),
     url(r'^features/', include('features.urls')),
     url(r'^bugs/', include('bugs.urls')),
+    url(r'^blog/', include('blog.urls'))
 ]
